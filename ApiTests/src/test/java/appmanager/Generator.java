6820895fc6c63140521aea6229b843558f0196a1
gorelov-m-v <123184098+gorelov-m-v@users.noreply.github.com>
2023-04-13 21:23:42 +0300

    Переработал генератор паролей

--- ApiTests/src/test/java/appmanager/Generator.java
+++ ApiTests/src/test/java/appmanager/Generator.java
@@ -1,5 +1,11 @@
 package appmanager;
 
+import org.apache.commons.lang3.RandomUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 
 public class Generator extends HelperBase {
@@ -8,9 +14,12 @@ public class Generator extends HelperBase {
         super(app);
     }
 
-    private final String LOWER = "abcdefghijklmnopqrstuvwxyz";
-    private final String UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    private final String DIGITS = "0123456789";
+    private static final String LOWER = "abcdefghijklmnopqrstuvwxyz";
+    private static final String UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String DIGITS = "0123456789";
+    private static final String SPECIAL = "~!@#$%^&*_-+=`|\\(){}[]:;\"'<>,.?/";
+    public static final String ALPHANUMERIC_AND_SPECIAL_SYMBOLS = UPPER + LOWER + DIGITS + SPECIAL;
+
     public String[] CURRENCY  = {"RUR", "USD", "EUR", "KZT"};
     public String api_key = "sasa";
     public String simplePassword = "Aa123456!";
@@ -31,14 +40,31 @@ public class Generator extends HelperBase {
         return randomEmail.toLowerCase();
     }
 
-    public String randomPassword(int length) {
-        String suffix = "Aa!1";
-        int randomPathLength = length - suffix.length();
-        if(length == 0) {
-            return "";
+    public static String randomPassword(int length) {
+        List<Character> chars = new ArrayList<>(length);
+        boolean hasUpper = false;
+        boolean hasLower = false;
+        boolean hasNumber = false;
+        boolean hasSpecial = false;
+        for (int i = 0; i < length; i++) {
+            if (!hasUpper) {
+                chars.add(UPPER.charAt(RandomUtils.nextInt(0, UPPER.length())));
+                hasUpper = true;
+            } else if (!hasLower) {
+                chars.add(LOWER.charAt(RandomUtils.nextInt(0, LOWER.length())));
+                hasLower = true;
+            } else if (!hasNumber) {
+                chars.add(DIGITS.charAt(RandomUtils.nextInt(0, DIGITS.length())));
+                hasNumber = true;
+            } else if (!hasSpecial) {
+                chars.add(SPECIAL.charAt(RandomUtils.nextInt(0, SPECIAL.length())));
+                hasSpecial = true;
+            } else {
+                chars.add(ALPHANUMERIC_AND_SPECIAL_SYMBOLS.charAt(RandomUtils.nextInt(0, ALPHANUMERIC_AND_SPECIAL_SYMBOLS.length())));
+            }
         }
-        String randomPassword = randomString(randomPathLength) + suffix;
-        return randomPassword;
+        Collections.shuffle(chars);
+        return StringUtils.join(chars, "");
     }
 
     public String randomAccountName(int length) {
@@ -48,4 +74,5 @@ public class Generator extends HelperBase {
     public int randomValue(int min, int max) {
         return random.nextInt(max - min) + min;
     }
+
 }
